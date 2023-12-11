f14826dd2d80d1012ac5606711918a4536b81119
Mike White <myquite@gmail.com>
2023-12-11 12:34:48 -0600

    Update item management UI and functionality

--- script.js
+++ script.js
@@ -22,6 +22,18 @@ function loadItems() {
   }
 }
 
+function loadInput() {
+  const itemInput = document.getElementById("itemInput");
+  itemInput.focus();
+  itemInput.select();
+  itemInput.addEventListener("keyup", (event) => {
+    if (event.key === "Enter") {
+      console.log("Item Created");
+      createItem();
+    }
+  });
+}
+
 /**
  * Creates a new item and adds it to the items array.
  * Clears the input field, saves the items, and displays them.
@@ -43,15 +55,18 @@ function displayItems() {
   const itemsList = document.getElementById("itemsList");
   itemsList.innerHTML = "";
   items.forEach((item, index) => {
-    itemsList.innerHTML += `<li>${item}</li>`;
+    itemsList.innerHTML += `
+      <li>
+        <div class="item">
+          <span>${item}</span>
+        </div>
+        <div class="buttons">
+          <button class="update" onclick="updateItem(${index})">✎</button>
+          <button class="delete" onclick="deleteItem(${index})">ｘ</button>
+        </div>
+      </li>
+    `;
   });
-}
-
-/**
- * Shows the update section by setting its display property to "block".
- */
-function showUpdateSection() {
-  document.getElementById("updateSection").style.display = "block";
 }
 
 function cancelUpdate() {
@@ -63,27 +78,57 @@ function cancelUpdate() {
  * Finds the index of the current item, replaces it with the new item,
  * saves the items, and displays them.
  */
-function updateItem() {
-  const currentItem = document.getElementById("currentItem").value;
-  const newItem = document.getElementById("newItem").value;
-  const index = items.indexOf(currentItem);
-  if (index !== -1) {
+function updateItem(index) {
+  // Build the form into the list item
+  const itemsList = document.getElementById("itemsList");
+  const item = items[index];
+  itemsList.innerHTML = `
+    <li>
+      <div class="item">
+        <input id="editInput" type="text" value="${item}">
+      </div>
+      <div class="buttons"> 
+        <button class="delete" onclick="cancelUpdate()">Cancel</button>
+      </div>
+    </li>
+  `;
+
+  // update the item with new input
+  setTimeout(() => {
+    // wait for the DOM to update
+    const editInput = document.getElementById("editInput");
+    editInput.focus();
+    editInput.select();
+    editInput.addEventListener("keyup", (event) => {
+      if (event.key === "Enter") {
+        console.log("Item Updated");
+        saveUpdate(index);
+      }
+    });
+  }, 150);
+}
+
+saveUpdate = (index) => {
+  const editInput = document.getElementById("editInput");
+  const newItem = editInput.value;
+  if (newItem) {
     items[index] = newItem;
     saveItems();
     displayItems();
   }
-  document.getElementById("currentItem").value = "";
-  document.getElementById("newItem").value = "";
-}
+};
+
+cancelUpdate = () => {
+  displayItems();
+};
 
 /**
  * Deletes an item from the items array.
  * Finds the index of the item, removes it from the array,
  * saves the items, and displays them.
  */
-function deleteItem() {
+function deleteItem(index) {
   const itemInput = document.getElementById("itemInput");
-  const index = items.indexOf(itemInput.value);
   if (index !== -1) {
     items.splice(index, 1);
     saveItems();
@@ -93,5 +138,6 @@ function deleteItem() {
 }
 
 // Initial display
+loadInput();
 loadItems();
 displayItems();
